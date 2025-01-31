727f00b8616878c722d717b4c06d6ce91c1bb26f
AnaJZP <ana.jzp@gmail.com>
2025-01-31 14:27:54 -0600

    Update 02_Modelo.py

--- pages/02_Modelo.py
+++ pages/02_Modelo.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import streamlit as st
 import torch
 from model import SentimentLSTM
@@ -5,9 +10,7 @@ from transformers import AutoTokenizer
 from deep_translator import GoogleTranslator
 import numpy as np
 from eda import SENTIMENT_MAP, cargar_datos
-import plotly.express as px
 import pandas as pd
-from collections import Counter
 
 st.set_page_config(page_title="Modelo de Sentimientos", layout="wide")
 
